26489d61481ecf317d3f9065abdd14a92f62721c
Sumi451 <sharmasumiran12@gmail.com>
2025-05-27 13:39:04 +0530

    modified:   RPS/src/RockPaperScissor.java

--- RPS/src/RockPaperScissor.java
+++ RPS/src/RockPaperScissor.java
@@ -1,3 +1,19 @@
-public class RockPaperScissor {
-    
+//will handle the logic of the game
+public class RockPaperScissor
+{
+    public static final String[] computerChoices= {"Rock", "Paper", "Scissors"};
+
+    //generate a random number to choose a random choice
+    private Random random;
+
+    //initialize random obj
+    public RockPaperScissor()
+    {
+        random= new Random();
+    }
+    //call this to start playing the game
+    public String playRockPaperScissor(String playerChoice)
+    {
+        
+    }
 }
